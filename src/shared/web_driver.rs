41851adb71ed7eac9ff930e4168a6abc93889fae
Rebekah <rwaterbury@gmail.com>
2023-11-30 23:54:40 -0800

    a little change

--- src/shared/web_driver.rs
+++ src/shared/web_driver.rs
@@ -12,7 +12,9 @@ pub async fn create(browser: &str) -> Result<WebDriver, WebDriverError> {
       prefs.set("browser.download.manager.showWhenStarting", false);
       prefs.set("browser.download.dir", "/Users/rwaterbury/dev/rust/tmp/docs".to_string());
       prefs.set("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream".to_string());
-  
+      prefs.set("acceptInsecureCerts", true);
+      prefs.set("marionette", false);
+      
       let mut caps = FirefoxCapabilities::new();
       caps.set_preferences(prefs)?;
       let driver = WebDriver::new("http://localhost:4444", caps).await?;
